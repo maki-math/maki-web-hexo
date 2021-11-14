197c9eecbb2c7cd1fe206f096a22eaeb33b72adf
Sg <suica@users.noreply.github.com>
2021-11-14 20:51:25 +0800

    feat: add @emotion/styled

--- src/modules/App/App.tsx
+++ src/modules/App/App.tsx
@@ -1,44 +1,15 @@
-import React, { useState } from 'react';
-import logo from '@/assets/logo.svg';
-import '@/modules/App/App.css';
 import { Button } from 'antd';
+import 'antd/dist/antd.css';
+import React, { useState } from 'react';
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <Button onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </Button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div>
+      <Button onClick={() => setCount((count) => count + 1)}>
+        count is: {count}
+      </Button>
     </div>
   );
 }
