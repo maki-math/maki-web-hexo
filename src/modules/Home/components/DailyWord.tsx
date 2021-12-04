8e9ef68b1071f48045523264d94b3f3d650a0ad8
Sg <suica@users.noreply.github.com>
2021-12-04 22:12:13 +0800

    fix(home): refer the png using import statement

--- src/modules/Home/components/DailyWord.tsx
+++ src/modules/Home/components/DailyWord.tsx
@@ -1,16 +1,12 @@
 import { Card, Row } from 'antd';
 import React from 'react';
+import IdeaPng from '@/assets/idea.png';
 const Text = `"九万里风鹏正举。风休住，蓬舟吹取三山去。" ——  【宋】 李清照`;
 export function DailyWord() {
   return (
     <Card bodyStyle={{ height: '100px' }}>
       <Row className="h-full" justify="center" align="middle">
-        <img
-          src="/src/assets/idea.png"
-          alt=""
-          height={60}
-          style={{ marginRight: '20px' }}
-        />
+        <img src={IdeaPng} alt="" height={60} style={{ marginRight: '20px' }} />
         {Text}
       </Row>
     </Card>
