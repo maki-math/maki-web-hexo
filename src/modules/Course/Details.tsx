fe30efee589993efea956301a77a8af8bcb8b1b7
Sg <suica@users.noreply.github.com>
2022-02-26 21:25:24 +0800

    feat(course detail): change cc to fc

--- src/modules/Course/Details.tsx
+++ src/modules/Course/Details.tsx
@@ -1,5 +1,4 @@
 // eslint-disable-next-line
-// @ts-nocheck
 import React from 'react';
 import {
   Row,
@@ -17,94 +16,87 @@ const { Title, Paragraph, Text, Link } = Typography;
 const blockContent = `AntV 是蚂蚁金服全新一代数据可视化解决方案，致力于提供一套简单方便、专业可靠、不限可能的数据可视化最佳实践。得益于丰富的业务场景和用户需求挑战，AntV 经历多年积累与不断打磨，已支撑整个阿里集团内外 20000+ 业务系统，通过了日均千万级 UV 产品的严苛考验。
 我们正在基础图表，图分析，图编辑，地理空间可视化，智能可视化等各个可视化的领域耕耘，欢迎同路人一起前行。`;
 
-class Details extends React.Component {
-  render() {
-    return (
-      <div>
-        <Row align="middle">
-          <Col span={4} offset={2}>
-            <Image
-              width={160}
-              height={200}
-              preview={false}
-              src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
-            />
-          </Col>
-          <Col span={16}>
-            <Descriptions title="User Info" column={1}>
-              <Descriptions.Item label="UserName">
-                Zhou Maomao
-              </Descriptions.Item>
-              <Descriptions.Item label="Telephone">
-                1810000000
-              </Descriptions.Item>
-              <Descriptions.Item label="Live">
-                Hangzhou, Zhejiang
-              </Descriptions.Item>
-              <Descriptions.Item label="Address">
-                No. 18, Wantang Road, Xihu District, Hangzhou, Zhejiang, China
-                No. 18, Wantang Road, Xihu District, Hangzhou, Zhejiang, China
-                No. 18, Wantang Road, Xihu District, Hangzhou, Zhejiang, China
-                No. 18, Wantang Road, Xihu District, Hangzhou, Zhejiang, China
-                No. 18, Wantang Road, Xihu District, Hangzhou, Zhejiang, China
-              </Descriptions.Item>
-            </Descriptions>
-            ,
-          </Col>
-        </Row>
-        <Row>
-          <Col span={20} offset={2}>
-            <Typography>
-              <Divider />
+export function Details() {
+  return (
+    <div>
+      <Row align="middle">
+        <Col span={4} offset={2}>
+          <Image
+            width={160}
+            height={200}
+            preview={false}
+            src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
+          />
+        </Col>
+        <Col span={16}>
+          <Descriptions title="User Info" column={1}>
+            <Descriptions.Item label="UserName">Zhou Maomao</Descriptions.Item>
+            <Descriptions.Item label="Telephone">1810000000</Descriptions.Item>
+            <Descriptions.Item label="Live">
+              Hangzhou, Zhejiang
+            </Descriptions.Item>
+            <Descriptions.Item label="Address">
+              No. 18, Wantang Road, Xihu District, Hangzhou, Zhejiang, China No.
+              18, Wantang Road, Xihu District, Hangzhou, Zhejiang, China No. 18,
+              Wantang Road, Xihu District, Hangzhou, Zhejiang, China No. 18,
+              Wantang Road, Xihu District, Hangzhou, Zhejiang, China No. 18,
+              Wantang Road, Xihu District, Hangzhou, Zhejiang, China
+            </Descriptions.Item>
+          </Descriptions>
+          ,
+        </Col>
+      </Row>
+      <Row>
+        <Col span={20} offset={2}>
+          <Typography>
+            <Divider />
 
-              <Title>介绍</Title>
+            <Title>介绍</Title>
 
-              <Paragraph>
-                随着商业化的趋势，越来越多的企业级产品对更好的用户体验有了进一步的要求。带着这样的一个终极目标，我们（蚂蚁金服体验技术部）经过大量的项目实践和总结，逐步打磨出一个服务于企业级产品的设计体系
-                Ant Design。基于<Text mark>『确定』和『自然』</Text>
-                的设计价值观，通过模块化的解决方案，降低冗余的生产成本，让设计者专注于
-                <Text strong>更好的用户体验</Text>。
-              </Paragraph>
-              <Title level={2}>设计资源</Title>
-              <Paragraph>
-                我们提供完善的设计原则、最佳实践和设计资源文件（
-                <Text code>Sketch</Text> 和<Text code>Axure</Text>
-                ），来帮助业务快速设计出高质量的产品原型。
-              </Paragraph>
+            <Paragraph>
+              随着商业化的趋势，越来越多的企业级产品对更好的用户体验有了进一步的要求。带着这样的一个终极目标，我们（蚂蚁金服体验技术部）经过大量的项目实践和总结，逐步打磨出一个服务于企业级产品的设计体系
+              Ant Design。基于<Text mark>『确定』和『自然』</Text>
+              的设计价值观，通过模块化的解决方案，降低冗余的生产成本，让设计者专注于
+              <Text strong>更好的用户体验</Text>。
+            </Paragraph>
+            <Title level={2}>设计资源</Title>
+            <Paragraph>
+              我们提供完善的设计原则、最佳实践和设计资源文件（
+              <Text code>Sketch</Text> 和<Text code>Axure</Text>
+              ），来帮助业务快速设计出高质量的产品原型。
+            </Paragraph>
 
-              <Paragraph>
-                <ul>
-                  <li>
-                    <Link href="/docs/spec/proximity-cn">设计原则</Link>
-                  </li>
-                  <li>
-                    <Link href="/docs/spec/overview-cn">设计模式</Link>
-                  </li>
-                  <li>
-                    <Link href="/docs/resources-cn">设计资源</Link>
-                  </li>
-                </ul>
-              </Paragraph>
+            <Paragraph>
+              <ul>
+                <li>
+                  <Link href="/docs/spec/proximity-cn">设计原则</Link>
+                </li>
+                <li>
+                  <Link href="/docs/spec/overview-cn">设计模式</Link>
+                </li>
+                <li>
+                  <Link href="/docs/resources-cn">设计资源</Link>
+                </li>
+              </ul>
+            </Paragraph>
 
-              <Paragraph>
-                <blockquote>{blockContent}</blockquote>
-                <pre>{blockContent}</pre>
-              </Paragraph>
-            </Typography>
-            ,
-          </Col>
-        </Row>
-        <Row>
-          <Col offset={2} span={20}>
-            <Title>文章目錄</Title>
-            <SearchTree />
-          </Col>
-        </Row>
-      </div>
-    );
-  }
+            <Paragraph>
+              <blockquote>{blockContent}</blockquote>
+              <pre>{blockContent}</pre>
+            </Paragraph>
+          </Typography>
+          ,
+        </Col>
+      </Row>
+      <Row>
+        <Col offset={2} span={20}>
+          <Title>文章目錄</Title>
+          <SearchTree />
+        </Col>
+      </Row>
+    </div>
+  );
 }
-export { Details };
 
 // 目录组件
 const { Search } = Input;
