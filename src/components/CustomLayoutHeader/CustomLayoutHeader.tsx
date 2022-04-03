ad819d13dfd68246ddfea0b25ae8bbae7df77bf0
Sg <suica@users.noreply.github.com>
2022-04-04 02:12:12 +0800

    feat: add shadow to navbar

--- src/components/CustomLayoutHeader/CustomLayoutHeader.tsx
+++ src/components/CustomLayoutHeader/CustomLayoutHeader.tsx
@@ -6,4 +6,5 @@ export const CustomLayoutHeader = styled(Header)`
   background-color: var(--bg-color);
   display: flex;
   justify-content: space-between;
+  box-shadow: 0 2px 2px -2px rgba(0, 0, 0, 0.2);
 `;
