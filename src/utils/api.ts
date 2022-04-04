09516f7f8b1382b403c1b00a29db08857ff40c2f
Sg <suica@users.noreply.github.com>
2022-04-04 18:11:00 +0800

    feat(auth): implement login

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,7 +1,22 @@
 import { Api } from '@/generated-api/Api';
+import { AxiosRequestConfig } from 'axios';
+import { getToken } from './auth-token';
 
 const baseURL = import.meta.env.VITE_API_BASE_URL;
 
-export const api = new Api({
+const config: AxiosRequestConfig = {
   baseURL,
+};
+
+export const api = new Api(config);
+
+api.instance.interceptors.request.use((config) => {
+  const token = getToken();
+  if (token) {
+    if (!config.headers) {
+      config.headers = {};
+    }
+    config.headers.Authorization = `Token ${token}`;
+  }
+  return config;
 });
