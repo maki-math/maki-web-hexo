5d87ab41a2e53fa953411c87e382ecbdfc64bd6a
Sg <suica@users.noreply.github.com>
2022-04-04 16:04:56 +0800

    feat(auth): generate auth apis

--- src/generated-api/Api.ts
+++ src/generated-api/Api.ts
@@ -63,6 +63,21 @@ export interface QuestionSetNodeModel {
   id?: number;
 }
 
+export interface UserDetailsModel {
+  pk?: number;
+
+  /**
+   * 必填；长度为150个字符或以下；只能包含字母、数字、特殊字符“@”、“.”、“-”和“_”。
+   * @pattern ^[\w.@+-]+\z
+   */
+  username: string;
+
+  /** @format email */
+  email?: string;
+  first_name?: string;
+  last_name?: string;
+}
+
 export interface CourseModel {
   id?: number;
   category?: { id?: number; alias: string; name: string }[];
@@ -104,6 +119,31 @@ export interface CourseGalleryModel {
     contact?: string;
     created_at?: string;
   }[];
+}
+
+export interface PasswordResetModel {
+  /** @format email */
+  email: string;
+}
+
+export interface PasswordResetConfirmModel {
+  new_password1: string;
+  new_password2: string;
+  uid: string;
+  token: string;
+}
+
+export interface LoginModel {
+  username?: string;
+
+  /** @format email */
+  email?: string;
+  password: string;
+}
+
+export interface PasswordChangeModel {
+  new_password1: string;
+  new_password2: string;
 }
 
 export type QueryParamsType = Record<string | number, any>;
@@ -867,6 +907,168 @@ export class Api<
       this.request<void, any>({
         path: `/question_set/${id}/`,
         method: 'DELETE',
+        ...params,
+      }),
+  };
+  auth = {
+    /**
+     * @description Calls Django logout method and delete the Token object assigned to the current User object. Accepts/Returns nothing.
+     *
+     * @tags auth
+     * @name ListLogouts
+     * @request GET:/auth/logout/
+     */
+    listLogouts: (params: RequestParams = {}) =>
+      this.request<any[], any>({
+        path: `/auth/logout/`,
+        method: 'GET',
+        format: 'json',
+        ...params,
+      }),
+
+    /**
+     * @description Calls Django logout method and delete the Token object assigned to the current User object. Accepts/Returns nothing.
+     *
+     * @tags auth
+     * @name CreateLogout
+     * @request POST:/auth/logout/
+     */
+    createLogout: (data: any, params: RequestParams = {}) =>
+      this.request<any, any>({
+        path: `/auth/logout/`,
+        method: 'POST',
+        body: data,
+        type: ContentType.Json,
+        format: 'json',
+        ...params,
+      }),
+
+    /**
+     * @description Reads and updates UserModel fields Accepts GET, PUT, PATCH methods. Default accepted fields: username, first_name, last_name Default display fields: pk, username, email, first_name, last_name Read-only fields: pk, email Returns UserModel fields.
+     *
+     * @tags auth
+     * @name RetrieveUserDetails
+     * @request GET:/auth/user/
+     */
+    retrieveUserDetails: (params: RequestParams = {}) =>
+      this.request<UserDetailsModel, any>({
+        path: `/auth/user/`,
+        method: 'GET',
+        format: 'json',
+        ...params,
+      }),
+
+    /**
+     * @description Reads and updates UserModel fields Accepts GET, PUT, PATCH methods. Default accepted fields: username, first_name, last_name Default display fields: pk, username, email, first_name, last_name Read-only fields: pk, email Returns UserModel fields.
+     *
+     * @tags auth
+     * @name UpdateUserDetails
+     * @request PUT:/auth/user/
+     */
+    updateUserDetails: (data: UserDetailsModel, params: RequestParams = {}) =>
+      this.request<UserDetailsModel, any>({
+        path: `/auth/user/`,
+        method: 'PUT',
+        body: data,
+        type: ContentType.Json,
+        format: 'json',
+        ...params,
+      }),
+
+    /**
+     * @description Reads and updates UserModel fields Accepts GET, PUT, PATCH methods. Default accepted fields: username, first_name, last_name Default display fields: pk, username, email, first_name, last_name Read-only fields: pk, email Returns UserModel fields.
+     *
+     * @tags auth
+     * @name PartialUpdateUserDetails
+     * @request PATCH:/auth/user/
+     */
+    partialUpdateUserDetails: (
+      data: UserDetailsModel,
+      params: RequestParams = {}
+    ) =>
+      this.request<UserDetailsModel, any>({
+        path: `/auth/user/`,
+        method: 'PATCH',
+        body: data,
+        type: ContentType.Json,
+        format: 'json',
+        ...params,
+      }),
+
+    /**
+     * @description Calls Django Auth PasswordResetForm save method. Accepts the following POST parameters: email Returns the success/fail message.
+     *
+     * @tags auth
+     * @name CreatePasswordReset
+     * @request POST:/auth/password/reset/
+     */
+    createPasswordReset: (
+      data: PasswordResetModel,
+      params: RequestParams = {}
+    ) =>
+      this.request<PasswordResetModel, any>({
+        path: `/auth/password/reset/`,
+        method: 'POST',
+        body: data,
+        type: ContentType.Json,
+        format: 'json',
+        ...params,
+      }),
+
+    /**
+     * @description Password reset e-mail link is confirmed, therefore this resets the user's password. Accepts the following POST parameters: token, uid, new_password1, new_password2 Returns the success/fail message.
+     *
+     * @tags auth
+     * @name CreatePasswordResetConfirm
+     * @request POST:/auth/password/reset/confirm/
+     */
+    createPasswordResetConfirm: (
+      data: PasswordResetConfirmModel,
+      params: RequestParams = {}
+    ) =>
+      this.request<PasswordResetConfirmModel, any>({
+        path: `/auth/password/reset/confirm/`,
+        method: 'POST',
+        body: data,
+        type: ContentType.Json,
+        format: 'json',
+        ...params,
+      }),
+
+    /**
+     * @description Check the credentials and return the REST Token if the credentials are valid and authenticated. Calls Django Auth login method to register User ID in Django session framework Accept the following POST parameters: username, password Return the REST Framework Token Object's key.
+     *
+     * @tags auth
+     * @name CreateLogin
+     * @request POST:/auth/login/
+     */
+    createLogin: (data: LoginModel, params: RequestParams = {}) =>
+      this.request<LoginModel, any>({
+        path: `/auth/login/`,
+        method: 'POST',
+        body: data,
+        type: ContentType.Json,
+        format: 'json',
+        ...params,
+      }),
+
+    /**
+     * @description Calls Django Auth SetPasswordForm save method. Accepts the following POST parameters: new_password1, new_password2 Returns the success/fail message.
+     *
+     * @tags auth
+     * @name CreatePasswordChange
+     * @request POST:/auth/password/change/
+     */
+    createPasswordChange: (
+      data: PasswordChangeModel,
+      params: RequestParams = {}
+    ) =>
+      this.request<PasswordChangeModel, any>({
+        path: `/auth/password/change/`,
+        method: 'POST',
+        body: data,
+        type: ContentType.Json,
+        format: 'json',
         ...params,
       }),
   };
