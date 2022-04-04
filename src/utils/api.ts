f241c5f350531656ea56aad28e371f9c2b7411e9
Sg <suica@users.noreply.github.com>
2022-04-04 16:15:40 +0800

    chore: use axios to make request
    chore: remove debugger

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,5 +1,7 @@
 import { Api } from '@/generated-api/Api';
 
-const baseUrl = import.meta.env.VITE_API_BASE_URL;
+const baseURL = import.meta.env.VITE_API_BASE_URL;
 
-export const api = new Api({ baseUrl });
+export const api = new Api({
+  baseURL,
+});
