f2bb5d52f4360d2b060142df836c464b98e1336f
JMx <jiaminxin@outlook.com>
2022-04-24 21:56:33 +0800

    fix(Content): change katex to MathJax
    - katex 需要配置才能支持 align 等环境, 且渲染字体等也不正常, 因此转用MathJax. 除非文档非常大, 否则后者速度足够.
    - Vditor中使用MathJax 渲染 align, equation 等环境需要以双美元符包裹, 这对typora很友好, 但是latex中, 这些环境是没有双美元符包裹的. 因此不能直接渲染.
    - footer 问题, 内容溢出.
    - 需要添加公式编号

--- src/components/Standard/StandardMDContainer.tsx
+++ src/components/Standard/StandardMDContainer.tsx
@@ -3,12 +3,28 @@ import React, { useRef } from 'react';
 import Vditor from 'vditor';
 import 'vditor/dist/index.css';
 
+function texFormat(text) {
+  text = text.replace(/\r\n/g, '-AAAAAAAAAAAAAAAA-');
+  // typora 中支持使用 \Q 来代替 \mathbb{Q}, 但 MathJax 不支持.
+  text = text.replace(/\\(Q|R|C|Z|N)\s/g, '\\mathbb{$1}');
+  // 给没有双$包裹的数学环境加上双$包裹, 否则不渲染
+  text = text.replace(/([^\$A\-]\s*(-AAAAAAAAAAAAAAAA-)*\s*?)(\\begin{(equation|align)\*?}.*?\\end{(equation|align)\*?})/g, '$1\r\n$$$$\r\n$3\r\n$$$$');
+  text = text.replace(/-AAAAAAAAAAAAAAAA-/g, '\r\n');
+  return text;
+}
+
 export const StandardMDContainer = ({ text }: { text?: string }) => {
   const ref = useRef<HTMLDivElement>(null);
 
   React.useEffect(() => {
     if (isNotNil(text) && ref.current) {
-      Vditor.preview(ref.current, text);
+      text = texFormat(text);
+      Vditor.preview(ref.current, text, {
+        math: {
+          engine: "MathJax"
+        }, 
+        after: () => {}
+      });
     }
   }, [ref.current]);
 
