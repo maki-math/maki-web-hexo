24c1aac149ead35f7936efd2a43888e8d32ebc7e
JMx <33108686+jiandandaoxingfu@users.noreply.github.com>
2022-07-03 15:14:42 +0800

    feat(questionSetNode): QuestionSetNode edit (#43)
    feat(question): add question delete and edit
    
    feat(questionSet): hide QuestionPage when no auth

--- src/modules/Questions/QuestionSetNodeEditingPage.tsx
+++ src/modules/Questions/QuestionSetNodeEditingPage.tsx
@@ -1,47 +1,145 @@
 import { StandardPageLayout } from '@/components/Standard/StandardPageLayout';
+import { QuestionSetNodeModel, PatchedQuestionSetNodeModel } from '@/generated-api/Api';
 import { api } from '@/utils/api';
-import { QuestionSetNodeModel } from '@/generated-api/Api';
 import { useRequest } from 'ahooks';
-import { Spin, Table } from 'antd';
-import React from 'react';
-import { Link, Route, useRouteMatch } from 'react-router-dom';
+import { Button, Col, Row, Table, Input, Form, message, Popconfirm, Space } from 'antd';
+import { default as React, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { useForm } from 'antd/lib/form/Form';
 
 interface Props {
-  questionNodeId: number;
+  questionNodeId?: number;
 }
 
 export function QuestionSetNodeEditingPage({ questionNodeId }: Props) {
-  const { data, loading } = useRequest(
+  const { data, loading, refresh } = useRequest(
     () => {
-      return api.questionSet.questionSetRetrieve(questionNodeId);
+      return questionNodeId 
+             ? api.questionSet.questionSetRetrieve(questionNodeId)
+             : api.questionSet.questionSetList();
     },
     { refreshDeps: [questionNodeId] }
   );
-
-  var questionSet = data?.data;
-  questionSet = questionSet?.id ? [questionSet] : questionSet;
+  const questionSetList = data?.data && (questionNodeId ? [data.data] : data.data);
+  const [questionSet, setQuestionSet] = useState({});
+  const [isEditing, setIsEditing] = useState(false);
 
   const columns = [
     {
-      title: '习题集名',
+      title: '习题集' + (questionNodeId ? '目录' : '名'),
       dataIndex: 'label',
       key: 'label',
       render: (label: string, row: QuestionSetNodeModel) => {
         const path = {
-          pathname: `/questions/${row?.question}`,
+          pathname: questionNodeId 
+                    ? `/questions/` + row.question
+                    : '/questions/sets/' + row.id,
         }
-        return row.children?.[0] ? <span>{label}</span> : <Link to={path}>{label}</Link>
+        return (!questionNodeId || row.question !== null)
+               ? <Link to={path}>{label}</Link>
+               : <span>{label}</span>
+      },
+    }, 
+    {
+      title: '操作',
+      dataIndex: 'id',
+      key: 'id',
+      render: (id: number, row: QuestionSetNodeModel) => {
+        return (
+          <Space>
+            <a disabled={isEditing} onClick={() => renameQuestionSet(row)}>重命名</a>
+            <a disabled={isEditing} onClick={() => addQuestionSet(row)}>添加</a>
+            <Popconfirm
+              title="确定要删除吗?"
+              onConfirm={() => deleteQuestionSet(row)}
+              onCancel={() => {}}
+              okText="确定"
+              cancelText="取消"
+            >
+              <a disabled={isEditing} href="#">删除</a>
+            </Popconfirm>
+        </Space>);
       },
     }
   ];
-  return (
-    <StandardPageLayout title={"习题集详情"}>
-      <Table
-        columns={columns}
-        dataSource={questionSet}
-        loading={loading}
-        rowKey="id"
-      />
-    </StandardPageLayout>
-  );
+
+  const uploadQuestionSet = (questionSet: PatchedQuestionSetNodeModel) => {
+    (questionSet.id
+      ? (questionSet.label
+        ? api.questionSet.questionSetPartialUpdate(questionSet.id, questionSet)
+        : api.questionSet.questionSetDestroy(questionSet.id)
+        )
+      : api.questionSet.questionSetCreate(questionSet)
+    )
+      .then((res) => {
+        message.success("更新成功");
+        refresh();
+      })
+      .catch((err) => {
+        message.error('上传失败, 请稍后重试.');
+      });
+  };
+  const { run } = useRequest(uploadQuestionSet, { manual: true });
+
+  const renameQuestionSet = (qs: QuestionSetNodeModel) => {
+    setQuestionSet(qs);
+    setIsEditing(true);
+  }
+
+  const addQuestionSet = (qs: QuestionSetNodeModel) => {
+    setIsEditing(true);
+  }
+
+  const deleteQuestionSet = (qs: QuestionSetNodeModel) => {
+    if (qs.children?.[0] || qs.question !== null) {
+      message.error("习题集非空, 无法删除!");
+    } else {
+      run({id: qs.id});
+    }
+  }
+
+  const onFormFinish = (formData: PatchedQuestionSetNodeModel) => {
+    if (formData.label) {
+      if (questionNodeId && !questionSet.label) { 
+        // To do: InsertQuestionSet
+      } else if (questionSet.label !== formData.label) {
+        questionSet.label = formData.label;
+        run(questionSet);
+      }
+    }
+    questionSet.id && setQuestionSet({});
+    form.resetFields();
+    setIsEditing(false);
+  }
+  
+  type QuestionSetFormData = PatchedQuestionSetNodeModel;
+  const [form] = useForm<QuestionSetFormData>();
+
+  return <>
+    {
+    isEditing &&
+    <Form
+      initialValues={questionSet}
+      form={form}
+      layout={"inline"}
+      onFinish={(values) => onFormFinish(values)}
+    >
+      <Form.Item name="label">
+        <Input placeholder="请输入习题集名" style={{ width: "400px" }} autoFocus/>
+      </Form.Item>
+      <Form.Item>
+        <Button type="primary" htmlType="submit" disabled={loading}>
+          {loading ? "上传中" : (questionSet.id ? "修改/取消" : "添加/取消")}
+        </Button>
+      </Form.Item>
+    </Form>
+    }
+    <Table
+      columns={columns}
+      dataSource={questionSetList}
+      loading={loading}
+      rowKey="id"
+      expandable={questionNodeId ? {} : { childrenColumnName: 'N/A' }}
+    />
+  </>
 }
--- src/modules/Questions/QuestionsPage.tsx
+++ src/modules/Questions/QuestionsPage.tsx
@@ -1,23 +1,31 @@
 import { StandardPageLayout } from '@/components/Standard/StandardPageLayout';
 import { TagsDisplay } from '@/components/Standard/TagsDisplay';
-import { QuestionModel, QuestionSetNodeModel } from '@/generated-api/Api';
+import { QuestionModel } from '@/generated-api/Api';
 import { api } from '@/utils/api';
 import { useRequest } from 'ahooks';
-import { Button, Col, Row, Space, Table } from 'antd';
+import { Button, Col, Row, Table, message, Popconfirm, Space } from 'antd';
 import { default as React, FC } from 'react';
 import { Link, Route, Switch } from 'react-router-dom';
-import { QuestionEditingPage } from './QuestionEditingPage';
 import { QuestionSetNodeEditingPage } from './QuestionSetNodeEditingPage';
+import { QuestionEditingPage } from './QuestionEditingPage';
 import { QuestionDetailPage } from './QuestionDetailPage';
 import moment from 'moment';
-import { AuthModuleEnum, useAuth, useIsLoggedIn } from '@/utils/auth-token';
-import { isNotNil } from '@/utils/types';
 
 export function QuestionList() {
-  const { data, loading } = useRequest(api.question.questionList);
+  const { data, loading, refresh } = useRequest(api.question.questionList);
   const questions = data?.data;
 
-  const isAuthed = useAuth(AuthModuleEnum.QuestionPage);
+  const deleteQuestion = (question: QuestionModel) => {
+     api.question.questionDestroy(question.id)
+      .then((res) => {
+        message.success("删除成功");
+        refresh();
+      })
+      .catch((err) => {
+        message.error("删除失败, 请稍后重试.");
+      });
+  };
+  const { run } = useRequest(deleteQuestion, { manual: true });
 
   const columns = [
     {
@@ -50,7 +58,7 @@ export function QuestionList() {
       dataIndex: 'created_at',
       key: 'created_at',
       render: (date: string) => {
-        return <span>{moment(date).format('YYYY-MM-DD')}</span>;
+        return <span>{moment(date).format('YYYY-MM-DD')}</span>
       },
     },
     {
@@ -58,82 +66,79 @@ export function QuestionList() {
       dataIndex: 'updated_at',
       key: 'updated_at',
       render: (date: string) => {
-        return <span>{moment(date).format('YYYY-MM-DD')}</span>;
+        return <span>{moment(date).format('YYYY-MM-DD')}</span>
       },
     },
-    isAuthed
-      ? {
-          title: '操作',
-          dataIndex: 'id',
-          key: 'id',
-          render: (id: number) => {
-            const path = {
-              pathname: '/questions/edit/' + id,
-            };
-            return (
-              <Space>
-                <Link to={path}>编辑</Link>
-              </Space>
-            );
-          },
-        }
-      : null,
-  ].filter(isNotNil);
-
-  return <Table columns={columns} dataSource={questions} loading={loading} />;
-}
-
-export function QuestionSetList() {
-  const { data, loading } = useRequest(api.questionSet.questionSetList);
-  const questionSet = data?.data;
-
-  const columns = [
     {
-      title: '习题集名',
-      dataIndex: 'label',
-      key: 'label',
-      render: (label: string, row: QuestionSetNodeModel) => {
+      title: '操作',
+      dataIndex: 'id',
+      key: 'id',
+      render: (id: number, row: QuestionModel) => {
         const path = {
-          pathname: '/questions/sets/' + row.id,
+          pathname: '/questions/edit/' + id,
         };
-        return <Link to={path}>{label}</Link>;
+        return (
+          <Space>
+            <Link to={path}>编辑</Link>
+            <Popconfirm
+              title="确定要删除吗?"
+              onConfirm={() => run(row)}
+              onCancel={() => {}}
+              okText="确定"
+              cancelText="取消"
+            >
+              <a href="#">删除</a>
+            </Popconfirm>
+        </Space>);
       },
-    },
+    }
   ];
 
   return (
-    <Table
-      columns={columns}
-      dataSource={questionSet}
-      loading={loading}
-      rowKey="id"
-      expandable={{ childrenColumnName: 'N/A' }}
-    />
-  );
+    <StandardPageLayout
+      title="题目列表"
+      subTitle={<Link to="/questions/sets">查看习题集列表</Link>}
+    >
+      <Row gutter={[8, 8]}>
+        <Col span={24}>
+          <Link to="/questions/edit">
+            <Button type="primary">添加题目</Button>
+          </Link>
+        </Col>
+        <Col span={24}>
+          <Table columns={columns} dataSource={questions} loading={loading} />
+        </Col>
+      </Row>
+    </StandardPageLayout>
+  )
+}
+
+export function QuestionSetList() {
+  return (
+    <StandardPageLayout
+      title="习题集列表"
+      subTitle={<Link to="/questions">查看题目列表</Link>}
+    >
+      <QuestionSetNodeEditingPage></QuestionSetNodeEditingPage>
+    </StandardPageLayout>
+  )
+}
+
+export function QuestionSetDetail({id}: {id: string}) {
+  return (
+    <StandardPageLayout title={"习题集详情"}>
+      <QuestionSetNodeEditingPage
+        questionNodeId={id}
+      ></QuestionSetNodeEditingPage>
+     </StandardPageLayout>
+  )
 }
 
 export const QuestionsPage: FC<unknown> = () => {
-  const isAuthed = useAuth(AuthModuleEnum.QuestionPage);
   return (
     <Switch>
       <Route path="/questions" exact>
-        <StandardPageLayout
-          title="题目列表"
-          subTitle={<Link to="/questions/sets">查看习题集列表</Link>}
-        >
-          <Row gutter={[8, 8]}>
-            {isAuthed && (
-              <Col span={24}>
-                <Link to="/questions/edit">
-                  <Button type="primary">添加题目</Button>
-                </Link>
-              </Col>
-            )}
-            <Col span={24}>
-              <QuestionList></QuestionList>
-            </Col>
-          </Row>
-        </StandardPageLayout>
+        <QuestionList></QuestionList>
       </Route>
       <Route path="/questions/edit" exact>
         <QuestionEditingPage id={0}></QuestionEditingPage>
@@ -142,28 +147,23 @@ export const QuestionsPage: FC<unknown> = () => {
         path="/questions/edit/:id"
         render={(props) => {
           return (
-            <QuestionEditingPage
-              id={Number(props.match.params.id)}
+            <QuestionEditingPage 
+              id={Number(props.match.params.id)} 
             ></QuestionEditingPage>
           );
         }}
       ></Route>
       <Route path="/questions/sets">
         <Route path="/questions/sets" exact>
-          <StandardPageLayout
-            title="习题集列表"
-            subTitle={<Link to="/questions">查看题目列表</Link>}
-          >
-            <QuestionSetList></QuestionSetList>
-          </StandardPageLayout>
+          <QuestionSetList></QuestionSetList>
         </Route>
         <Route
           path="/questions/sets/:id"
           render={(props) => {
             return (
-              <QuestionSetNodeEditingPage
-                questionNodeId={Number(props.match.params.id)}
-              ></QuestionSetNodeEditingPage>
+              <QuestionSetDetail 
+                id={Number(props.match.params.id)}
+              ></QuestionSetDetail>
             );
           }}
         ></Route>
@@ -172,7 +172,9 @@ export const QuestionsPage: FC<unknown> = () => {
         path="/questions/:id"
         render={(props) => {
           return (
-            <QuestionDetailPage id={props.match.params.id}></QuestionDetailPage>
+            <QuestionDetailPage 
+              id={props.match.params.id} 
+            ></QuestionDetailPage>
           );
         }}
       ></Route>
