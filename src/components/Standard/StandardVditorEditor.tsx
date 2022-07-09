a4c33c0c71720587ba6affa7d9e62b9b9a630e5d
Sg <suica@users.noreply.github.com>
2022-07-09 19:09:48 +0800

    fix(Vditor): fix type errors & remove unnecessary features from toolbar

--- src/components/Standard/StandardVditorEditor.tsx
+++ src/components/Standard/StandardVditorEditor.tsx
@@ -4,25 +4,76 @@ import Vditor from 'vditor';
 
 interface Props {
   id: string;
-  after?: () => void;
-  input?: () => void;
+  after?: (v: Vditor) => void;
+  input?: (x: string) => void;
 }
 
-export function VditorEditor ({id, after, input}: Props) {
+// default config from https://github.com/Vanessa219/vditor/blob/master/src/ts/util/Options.ts
+const ToolbarConfig = [
+  'emoji',
+  'headings',
+  'bold',
+  'italic',
+  'strike',
+  'link',
+  '|',
+  'list',
+  'ordered-list',
+  'check',
+  'outdent',
+  'indent',
+  '|',
+  'quote',
+  'line',
+  'code',
+  'inline-code',
+  'insert-before',
+  'insert-after',
+  '|',
+  'upload',
+  // 'record',
+  'table',
+  '|',
+  'undo',
+  'redo',
+  '|',
+  'fullscreen',
+  'edit-mode',
+  {
+    name: 'more',
+    toolbar: [
+      // 'both',
+      // 'code-theme',
+      // 'content-theme',
+      'export',
+      'outline',
+      'preview',
+      // 'devtools',
+      // 'info',
+      // 'help',
+    ],
+  },
+];
+
+export function VditorEditor({ id, after, input }: Props) {
   React.useEffect(() => {
-      const vditor = new Vditor(id, {
-        mode: 'ir',
-        minHeight: 150,
-        placeholder: '请输入...',
-        preview: {
-          math: {
-            engine: "MathJax"
-          }
+    const vditor: Vditor = new Vditor(id, {
+      mode: 'ir',
+      minHeight: 350,
+      placeholder: '请输入...',
+      preview: {
+        math: {
+          engine: 'MathJax',
         },
-        after: () => after?.(vditor),
-        input: (md) => input?.(md)
-      });
+      },
+      toolbar: ToolbarConfig,
+      after: () => after?.(vditor),
+      input: (md) => input?.(md),
+    });
+    return () => {
+      vditor.destroy();
+    };
   }, []);
 
-  return <div id={id} className="vditor edit"/>;
+  return <div id={id} className="vditor edit" />;
 }
