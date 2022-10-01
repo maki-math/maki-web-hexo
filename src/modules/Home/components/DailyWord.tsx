d4967ffdaaa473e8be4a074b978e51a0f33bf712
Sg <suica@users.noreply.github.com>
2022-10-01 20:30:34 +0800

    feat(mobile): support daily word

--- src/modules/Home/components/DailyWord.tsx
+++ src/modules/Home/components/DailyWord.tsx
@@ -4,7 +4,7 @@ import IdeaPng from '@/assets/idea.png';
 const Text = `"九万里风鹏正举。风休住，蓬舟吹取三山去。" ——  【宋】 李清照`;
 export function DailyWord() {
   return (
-    <Card bodyStyle={{ height: '100px' }}>
+    <Card>
       <Row className="h-full" justify="center" align="middle">
         <img src={IdeaPng} alt="" height={60} style={{ marginRight: '20px' }} />
         {Text}
