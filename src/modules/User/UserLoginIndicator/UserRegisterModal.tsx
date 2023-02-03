352edf7a020614d540c2d0dc94a30bd0e0b26175
Sg <suica@users.noreply.github.com>
2023-02-04 00:15:07 +0800

    fix: show fields error in register form

--- src/modules/User/UserLoginIndicator/UserRegisterModal.tsx
+++ src/modules/User/UserLoginIndicator/UserRegisterModal.tsx
@@ -1,13 +1,14 @@
-import { LoginModel } from '@/generated-api/Api';
+import { RegisterModel } from '@/generated-api/Api';
 import { api } from '@/utils/api';
 import {
-  useTokenContext,
-  usePermissionsContext,
   fetchPermissions,
+  usePermissionsContext,
+  useTokenContext,
 } from '@/utils/auth-token';
 import { useRequest } from 'ahooks';
-import { Form, Input, message, Modal } from 'antd';
+import { Form, Input, Modal, message } from 'antd';
 import { useForm } from 'antd/lib/form/Form';
+import { FieldData } from 'rc-field-form/lib/interface';
 import React, { useCallback } from 'react';
 
 interface Props {
@@ -15,39 +16,52 @@ interface Props {
   onClose?: () => void;
 }
 
-type RegisterFormData = LoginModel & { rememberMe: boolean };
+type RegisterFormData = RegisterModel & { rememberMe: boolean };
 
 export function UserRegisterModal({ visible, onClose }: Props) {
   const { setToken } = useTokenContext();
   const { setPermissions } = usePermissionsContext();
-  const setUserInfo = (data: LoginModel) => {
-    return api.auth
-      .authRegistrationCreate({
-        username: data.username ?? '',
-        password1: data.password,
-        password2: data.password,
-        email: data.email,
-      })
-      .then((res) => {
-        setToken(res.data.key);
-        fetchPermissions(setPermissions);
-        onClose?.();
-      })
-      .catch(() => {
-        message.error('注册失败，请重试');
-        // TODO 根据err内容在表单中展示错误信息
-      });
-  };
-  const { run, loading } = useRequest(setUserInfo, { manual: true });
   const [form] = useForm<RegisterFormData>();
+  const register = useCallback(
+    (data: RegisterFormData) => {
+      return api.auth
+        .authRegistrationCreate({
+          username: data.username ?? '',
+          password1: data.password1,
+          password2: data.password2,
+          email: data.email,
+        })
+        .then((res) => {
+          setToken(res.data.key);
+          fetchPermissions(setPermissions);
+          onClose?.();
+        })
+        .catch((err) => {
+          const errorObject = err?.response?.data as
+            | Record<string, string[]>
+            | undefined;
+          if (errorObject) {
+            const errorFields: FieldData[] = Object.entries(errorObject).map(
+              ([k, v]) => {
+                return { name: k, errors: v };
+              }
+            );
+            form.setFields(errorFields);
+          }
+          message.error(`注册失败`);
+        });
+    },
+    [form]
+  );
 
+  const { run, loading } = useRequest(register, { manual: true });
   const resetForm = useCallback(() => {
     form.resetFields();
   }, [form]);
 
   const valid_confirm_password = ({ getFieldValue }) => ({
-    validator(_, value) {
-      if (!value || getFieldValue('password') === value) {
+    validator(_: any, value: string | undefined) {
+      if (!value || getFieldValue('password1') === value) {
         return Promise.resolve();
       }
       return Promise.reject(new Error('两次输入的密码不一致'));
@@ -55,7 +69,7 @@ export function UserRegisterModal({ visible, onClose }: Props) {
   });
 
   const valid_username = () => ({
-    validator(_, value) {
+    validator(_: any, value: string | undefined) {
       if (
         !value ||
         (value?.replace(/[\w.@+-]/g, '') === '' && value?.length <= 150)
@@ -110,15 +124,15 @@ export function UserRegisterModal({ visible, onClose }: Props) {
 
         <Form.Item
           label="密码"
-          name="password"
+          name="password1"
           rules={[{ required: true, message: '请输入密码' }]}
         >
           <Input.Password />
         </Form.Item>
         <Form.Item
           label="确认密码"
-          name="confirm-password"
-          dependencies={['password']}
+          name="password2"
+          dependencies={['password1']}
           rules={[
             { required: true, message: '请重新输入密码' },
             valid_confirm_password,
