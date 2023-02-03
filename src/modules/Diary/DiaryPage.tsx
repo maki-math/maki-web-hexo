77120e1f5fb8432ee43702a5180f105d370fbaa0
Sg <suica@users.noreply.github.com>
2023-02-03 17:45:38 +0800

    feat: 增加解析和解析结果确认弹窗

--- src/modules/Diary/DiaryPage.tsx
+++ src/modules/Diary/DiaryPage.tsx
@@ -1,8 +1,9 @@
 import { StandardPageLayout } from '@/components/Standard/StandardPageLayout';
-import { Card, Col, Form, Row, Typography, message } from 'antd';
+import { Card, Col, Form, Modal, Row, Typography, message } from 'antd';
 import React, { useCallback } from 'react';
 import { DiaryEditor } from './DiaryEditor';
 import { DiaryHistory } from './DiaryHistory';
+import { parseDailyRecord } from './util/daily-record-parser';
 
 export interface DiaryFormData {
   content: string;
@@ -11,9 +12,37 @@ export interface DiaryFormData {
 export const DiaryPage = () => {
   const [form] = Form.useForm<DiaryFormData>();
 
-  const onFinish = useCallback(({ content }: DiaryFormData) => {
-    message.success('记录成功 ' + content);
-    form.resetFields();
+  const onFinish = useCallback(({ content: rawContent }: DiaryFormData) => {
+    const { parsed, unparsed } = parseDailyRecord(rawContent);
+    const successDetail = <Col span={24}>记录解析成功 {parsed.length} 条</Col>;
+    const failureDetail = (
+      <div>
+        解析失败{unparsed.length}条：
+        {unparsed.map((item) => {
+          return <Row>{item}</Row>;
+        })}
+        可更正后再试。
+      </div>
+    );
+    const content = (
+      <Row>
+        {successDetail}
+        {unparsed.length ? failureDetail : undefined}
+      </Row>
+    );
+    Modal.confirm({
+      title: '确认解析结果',
+      content,
+      async onOk() {
+        try {
+          // TODO call api here
+          message.success('记录成功');
+          form.resetFields();
+        } catch (error) {
+          message.error('记录失败，请联系管理员');
+        }
+      },
+    });
   }, []);
 
   return (
---
+++ src/modules/Diary/util/daily-record-parser.ts
@@ -0,0 +1,47 @@
+export type ParsedRecord = { record: string[] };
+export type UnparsedLine = string;
+export type DailyRecordParseResult = {
+  parsed: DailyRecordType[];
+  unparsed: UnparsedLine[];
+};
+
+// TODO use enum
+export type DailyRecordType = {
+  action: string;
+  objectName: string;
+  annotation: string | undefined;
+  unit: string;
+  quant: string;
+  objectType: string;
+  isIgnored?: boolean;
+};
+
+export function parseDailyRecord(s: string): DailyRecordParseResult {
+  /* eslint-disable no-useless-escape */
+  const DailyRecordPattern = /(?<action>编写|讲授|学习|挑错|讨论|刷题|绘制)(?<objectName>.*?)(?<objectType>讲义|习题集|课程|习题课|讲座|解题指南|笔记|词条|插图)\s*[:：]\s*(?<quant>[\d\.]+)(?<unit>[^（]*)(:?[（(](?<annotation>已完成)[)）])?/;
+  /* eslint-enable no-useless-escape */
+  const lines = s.split('\n');
+  const result: DailyRecordType[] = [];
+  const unparsed: UnparsedLine[] = [];
+  for (const line of lines) {
+    const trimmed = line.trim();
+    const groups = DailyRecordPattern.exec(trimmed)?.groups as
+      | DailyRecordType
+      | undefined;
+    if (groups) {
+      const { annotation } = groups;
+      const isIgnored = annotation?.includes('已完成');
+      result.push({ ...groups, isIgnored });
+    } else if (!isTemplateLine(line)) {
+      unparsed.push(line);
+    }
+  }
+  return {
+    parsed: result,
+    unparsed,
+  };
+}
+
+function isTemplateLine(s: string) {
+  return s.includes('日拱一卒');
+}
